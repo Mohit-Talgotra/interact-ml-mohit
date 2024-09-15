0f477a5ef17cd902f0a92a0897b9655a03800de0
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-09-16 01:03:35 +0530

    feat: code review integration fixes

--- controllers/code_reviewer.py
+++ controllers/code_reviewer.py
@@ -4,41 +4,28 @@ from code_review.src.fetcher.git_handler import GitHandler
 from code_review.src.fetcher.repository_manager import RepositoryManager
 
 
-def codeReview(body):
+def review_code(body):
     if body == None or body.repo_links == None:
         return {"message": "Repository links are required", "error": "Bad Request"}
 
     try:
-        return codeReviewer(body.repo_links)
+        repos = body.repo_links
+
+        cloneRepoPath = "code_review/cloned_repos"
+
+        git_handler = GitHandler()
+        repo_manager = RepositoryManager(git_handler)
+        chunk_extractor = ChunkExtractor2()
+        code_analyser = CodeAnalyser()
+
+        for repo in repos:
+            repo_manager.clone_repository(repo, cloneRepoPath)
+
+        chunk_extractor.processRepos(cloneRepoPath)
+        scores = code_analyser.processAllRepos(cloneRepoPath)
+        repo_manager.complete_cleanup()
+
+        return scores
 
     except Exception as e:
-        return {"message": f"Error executing script", "error": str(e)}
-
-
-def fetch_repository(url: str, base_path: str) -> str:
-    git_handler = GitHandler()
-    repo_manager = RepositoryManager(git_handler)
-
-    repo_manager.clone_repository(url, base_path)
-
-
-def codeReviewer(repos):
-    cloneRepoPath = "code_review/cloned_repos"
-
-    git_handler = GitHandler()
-    repo_manager = RepositoryManager(git_handler)
-    chunk_extractor = ChunkExtractor2()
-    code_analyser = CodeAnalyser()
-
-    for repo in repos:
-        fetch_repository(repo, cloneRepoPath)
-
-    chunk_extractor.processRepos(cloneRepoPath)
-
-    repo_manager.complete_cleanup()
-
-    print("Here")
-
-    # scores = code_analyser.processRepos(cloneRepoPath)
-
-    return []
+        return {"message": f"Error fetching code review", "error": str(e)}
